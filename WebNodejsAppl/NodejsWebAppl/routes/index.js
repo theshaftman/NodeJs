6916f9c595db73db5513641a640c7a066c570260
theshaftman <mariyan_apostolov@mail.bg>
2017-02-05 15:11:44 +0200

    Html project inclusion

--- WebNodejsAppl/NodejsWebAppl/routes/index.js
+++ WebNodejsAppl/NodejsWebAppl/routes/index.js
@@ -2,7 +2,11 @@
 /*
  * GET home page.
  */
+var fs = require("fs");
 
 exports.index = function (req, res) {
-    res.render('index', { title: 'Home' });
+    //res.render('index', { title: 'Home' });
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(fs.readFileSync("./views/index.html").toString());
+    res.end();
 };
